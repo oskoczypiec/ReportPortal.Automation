4836c04f71c68523af04e64c1d2301afb2a0ae73
Aleksandra Skoczypiec <aleksandr.skoczypiec@greystar.com>
2023-12-03 20:35:33 +0100

    added getfilters mock test

---
+++ ReportPortal.Automation/ReportPortal.Tests.API/Mock/FilterService.cs
@@ -0,0 +1,21 @@
+﻿using ReportPortal.Core.API.Models;
+
+namespace ReportPortal.Tests.API.Mock
+{
+    public class FilterService
+    {
+        private readonly IFilterApiClient _filterApi;
+
+        public FilterService(IFilterApiClient filterApi)
+        {
+            _filterApi = filterApi;
+        }
+
+        public Task <FiltersRootModel> GetFilters()
+        {
+            // Your implementation that calls the filter API
+            return _filterApi.GetFiltersAsync();
+        }
+    }
+}
+
---
+++ ReportPortal.Automation/ReportPortal.Tests.API/Mock/IFilterApiClient.cs
@@ -0,0 +1,9 @@
+﻿using ReportPortal.Core.API.Models;
+
+namespace ReportPortal.Tests.API.Mock
+{
+    public interface IFilterApiClient
+    {
+        public Task<FiltersRootModel> GetFiltersAsync();
+    }
+}
