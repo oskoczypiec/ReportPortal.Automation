045536441d16f94e4fe66541cd1365e520cd005a
Aleksandra Skoczypiec <aleksandr.skoczypiec@greystar.com>
2024-01-07 17:39:42 +0100

    more logging

--- ReportPortal.Automation/ReportPortal.Core/API/Services/RequestFactory.cs
+++ ReportPortal.Automation/ReportPortal.Core/API/Services/RequestFactory.cs
@@ -8,6 +8,7 @@ namespace ReportPortal.Core.API.Services
     using ReportPortal.Core.API.Models;
     using ReportPortal.Core.Config;
     using RestSharp;
+    using ReportPortal.Core.Logger;
 
     public class RequestFactory
     {
@@ -27,6 +28,7 @@ namespace ReportPortal.Core.API.Services
         public RestRequest GetRequest(string resource, Method method)
         {
             var responseGetUserToken = GetUserToken().Result;
+            Logger.Log.Info("User token: "+ responseGetUserToken);
             var token = JsonConvert.DeserializeObject<GetAuthModel>(responseGetUserToken.Content).AccessToken;
 
             var request = new RestRequest(resource, method);
@@ -36,6 +38,8 @@ namespace ReportPortal.Core.API.Services
 
         public async Task<RestResponse> GetUserToken()
         {
+            Logger.Log.Info("User name: " + Settings.User);
+            Logger.Log.Info("User pass: " + Settings.Pass);
             var request = new RestRequest("/uat/sso/oauth/token", Method.Post)
                 .AddHeader("Authorization", "Basic dWk6dWltYW4=")
                 .AddParameter("username", Settings.User)
